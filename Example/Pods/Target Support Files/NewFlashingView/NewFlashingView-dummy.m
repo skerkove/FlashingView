2e0108d3108a7011cf5c855444667a5dce85b089
Scott Kerkove <kerkoves@gmail.com>
2020-05-02 15:36:50 -0500

    initial

---
+++ Example/Pods/Target Support Files/NewFlashingView/NewFlashingView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NewFlashingView : NSObject
+@end
+@implementation PodsDummy_NewFlashingView
+@end
---
+++ Example/Pods/Target Support Files/Pods-NewFlashingView_Example/Pods-NewFlashingView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NewFlashingView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_NewFlashingView_Example
+@end
